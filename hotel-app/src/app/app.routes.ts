22a2192e062f68417513431e6def3e037ab9e3bf
benshabbat <benshabbat27@gmail.com>
2025-09-14 13:11:35 +0300

    feat: define application routes for Home, ReservationForm, and ReservationList components

--- hotel-app/src/app/app.routes.ts
+++ hotel-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home'; // הוסף ייבוא מתאים
+import { ReservationForm } from './reservation-form/reservation-form';
+import { ReservationList } from './reservation-list/reservation-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'new',
+    component: ReservationForm,
+  },
+  {
+    path: 'list',
+    component: ReservationList,
+  },
+];
