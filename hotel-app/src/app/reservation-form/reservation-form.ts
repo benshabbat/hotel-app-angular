615dc883eacd3518499c70d1b3dd73347154ca9f
benshabbat <benshabbat27@gmail.com>
2025-09-15 17:41:46 +0300

    feat: enhance reservation form with validation and error messages

--- hotel-app/src/app/reservation-form/reservation-form.ts
+++ hotel-app/src/app/reservation-form/reservation-form.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reservation-form.html',
-  styleUrl: './reservation-form.css'
+  styleUrl: './reservation-form.css',
 })
 export class ReservationForm {
+  reservationForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.reservationForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      checkIn: ['', Validators.required],
+      checkOut: ['', Validators.required],
+      roomNumber: ['', [Validators.required, Validators.min(1)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log(this.reservationForm.value);
+    }
+  }
 }
