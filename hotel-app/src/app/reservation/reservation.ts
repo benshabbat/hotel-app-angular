2a208dffca4c30698a8bb4f60a72f1c4a5e12948
benshabbat <benshabbat27@gmail.com>
2025-09-21 15:00:12 +0300

    fix: remove extra whitespace in import statement for Observable

--- hotel-app/src/app/reservation/reservation.ts
+++ hotel-app/src/app/reservation/reservation.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ReservationI } from '../models/reservation';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs  ';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
